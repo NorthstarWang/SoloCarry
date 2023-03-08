76cf4ad09bcb7981c3491f65a259cd8142b134eb
Yang Wang <1527638985@qq.com>
2023-03-08 12:19:40 -0700

    Feat: Chat

---
+++ app/src/main/java/com/example/solocarry/view/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.solocarry.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.solocarry.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
